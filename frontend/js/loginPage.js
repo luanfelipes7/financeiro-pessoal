22bbafe6f7c10b9c453fbb7ea01e5e13990fc190
Luan Felipe <luan.felipesev7n@gmail.com>
2026-07-30 22:10:55 -0300

    feat: telas de login e registro

---
+++ frontend/js/loginPage.js
@@ -0,0 +1,33 @@
+import {login} from './api.js';
+import {saveSession, isAuthenticated} from './storage.js';
+
+if(isAuthenticated()){
+    window.location.href = 'dashboard.html';
+}
+
+const form = document.getElementById('loginForm');
+const errorMessage = document.getElementById('errorMessage');
+const submitBtn = document.getElementById('submitBtn');
+
+form.addEventListener('submit', async (event) =>{
+    event.preventDefault();
+
+    errorMessage.classList.remove('visible');
+    submitBtn.disabled = true;
+    submitBtn.textContent = 'Entrando...';
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    try{
+        const data = await login ({email, password});
+        saveSession(data.token, data.user);
+        window.location.href = 'dashboard.html';
+    } catch (error){
+        errorMessage.textContent = errorMessage;
+        errorMessage.classList.add('visible');
+    } finally {
+        submitBtn.disabled = false;
+        submitBtn.textContent = 'Entrar';
+    }
+});
---
+++ frontend/js/registerPage.js
@@ -0,0 +1,31 @@
+import { register, login } from "./api.js";
+import {saveSession} from './storage.js';
+
+const form = document.getElementById('registerForm');
+const errorMessage = document.getElementById('errorMessage');
+const submitBtn = document.getElementById('submitBtn');
+
+form.addEventListener('submit', async(event) =>{
+    event.preventDefault();
+
+    errorMessage.classList.remove('visible');
+    submitBtn.disabled = true;
+    submitBtn.textContent = 'Criando conta...';
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    try{
+        await register({name, email, password});
+        const data = await login ({email, password});
+        saveSession(data.token, data.user);
+        window.location.href = 'dashboard.html';
+    } catch (error) {
+        errorMessage.textContent = errorMessage;
+        errorMessage.classList.add('visible');
+    } finally{
+        submitBtn.disabled = false;
+        submitBtn.textContent = 'Criar conta';
+    }
+});
